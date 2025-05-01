44525cf9d2442e94ba2552de2e6d3f1afdbb681e
vako291 <vasilixachiperadze8@gmail.com>
2025-05-01 18:12:36 +0400

    commit

---
+++ leson 03/classwork/cw.py
@@ -0,0 +1,30 @@
+print("va_sili")
+print("my_name")
+
+
+
+
+
+print("khachiperadze")
+print("myusername")
+
+
+
+# snake_case გამოყენების შემთხვევაში სიტყვას ეწერება ქვედა ტირე.
+# camelcase შემთხვევაში გადაბმულად იწერება სიტყვა და არა ქვედტირით
+# ანუ განსხვავებაა camelcase იწერება ქვედა ტირეს გარეშე  ხოლო snake_case ქვედატირეტი
+
+
+
+
+
+
+
+
+name = input("გამარჯობა შეიყვანე შენი სახელი:")
+
+username = input ("შეიყვანე შენი გვარი")
+
+
+print(name)
+print(username)
